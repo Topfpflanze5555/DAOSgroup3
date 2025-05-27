a6a25e961127107d4dda107e3f5208ae39a505d5
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2025-05-27 08:57:31 +0200

    create dao interface

--- src/main/java/DataLayer/DataAccessObjects/db/DAOS/PatientDaoSqlite.java
+++ src/main/java/DataLayer/DataAccessObjects/db/DAOS/PatientDaoSqlite.java
@@ -1,4 +1,33 @@
 package DataLayer.DataAccessObjects.db.DAOS;
 
-public class PatientDaoSqlite {
+import Models.Patient;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PatientDaoSqlite implements IDao<Patient> {
+    @Override
+    public Optional<Patient> get(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Patient> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public void save(Patient patient) {
+
+    }
+
+    @Override
+    public void update(Patient patient, String[] params) {
+
+    }
+
+    @Override
+    public void delete(Patient patient) {
+
+    }
 }
--- src/main/java/DataLayer/DataAccessObjects/db/DAOS/PflegekraftDaoSqlite.java
+++ src/main/java/DataLayer/DataAccessObjects/db/DAOS/PflegekraftDaoSqlite.java
@@ -1,4 +1,33 @@
 package DataLayer.DataAccessObjects.db.DAOS;
 
-public class PflegekraftDaoSqlite {
+import Models.Pflegekraft;
+
+import java.util.List;
+import java.util.Optional;
+
+public class PflegekraftDaoSqlite implements IDao<Pflegekraft> {
+    @Override
+    public Optional<Pflegekraft> get(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Pflegekraft> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public void save(Pflegekraft pflegekraft) {
+
+    }
+
+    @Override
+    public void update(Pflegekraft pflegekraft, String[] params) {
+
+    }
+
+    @Override
+    public void delete(Pflegekraft pflegekraft) {
+
+    }
 }

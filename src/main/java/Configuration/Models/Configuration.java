fab1c4098d360654593087e743acec16851f356a
Topfpflanze5555 <Lars.Wellbrock@ewe.net>
2025-05-29 20:45:17 +0200

    change Constructor Configuration.java

--- src/main/java/Configuration/Models/Configuration.java
+++ src/main/java/Configuration/Models/Configuration.java
@@ -7,11 +7,9 @@ public class Configuration {
     public enum MODELS { Leistung, Patient, Pflegekraf}
     public enum SAVEDTYPE {XML, CSV, SQLITE}
     private final HashMap<MODELS, SAVEDTYPE> configuration = new HashMap<>();
-    public Configuration(SAVEDTYPE configLeistung, SAVEDTYPE configPatient, SAVEDTYPE configPflegekraf)
+    public Configuration(HashMap<MODELS, SAVEDTYPE> configuration)
     {
-        configuration.put(MODELS.Leistung, configLeistung);
-        configuration.put(MODELS.Patient, configPatient);
-        configuration.put(MODELS.Pflegekraf, configPflegekraf);
+        this.configuration.putAll(configuration);
     }
 
     public HashMap<MODELS, SAVEDTYPE> getConfiguration() {

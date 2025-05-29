a71157309190efc3011da1b99802c3e35720cf47
Topfpflanze5555 <Lars.Wellbrock@ewe.net>
2025-05-29 20:46:19 +0200

    change Constructor and change typo Configuration.java

--- src/main/java/Configuration/Models/Configuration.java
+++ src/main/java/Configuration/Models/Configuration.java
@@ -4,7 +4,7 @@ package Configuration.Models;
 import java.util.HashMap;
 
 public class Configuration {
-    public enum MODELS { Leistung, Patient, Pflegekraf}
+    public enum MODELS { Leistung, Patient, Pflegekraft}
     public enum SAVEDTYPE {XML, CSV, SQLITE}
     private final HashMap<MODELS, SAVEDTYPE> configuration = new HashMap<>();
     public Configuration(HashMap<MODELS, SAVEDTYPE> configuration)

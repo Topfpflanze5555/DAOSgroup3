89853ae4bbd26ffe84eff4622f36159a200e7263
Topfpflanze5555 <Lars.Wellbrock@ewe.net>
2025-05-29 19:09:45 +0200

    Finnished Configuration.java

--- src/main/java/Configuration/Models/Configuration.java
+++ src/main/java/Configuration/Models/Configuration.java
@@ -1,3 +1,24 @@
 package Configuration.Models;
-    public class Configuration {
+
+
+import java.util.HashMap;
+
+public class Configuration {
+    public enum MODELS { Leistung, Patient, Pflegekraf}
+    public enum SAVEDTYPE {XML, CSV, SQLITE}
+    private final HashMap<MODELS, SAVEDTYPE> configuration = new HashMap<>();
+    public Configuration(SAVEDTYPE configLeistung, SAVEDTYPE configPatient, SAVEDTYPE configPflegekraf)
+    {
+        configuration.put(MODELS.Leistung, configLeistung);
+        configuration.put(MODELS.Patient, configPatient);
+        configuration.put(MODELS.Pflegekraf, configPflegekraf);
+    }
+
+    public HashMap<MODELS, SAVEDTYPE> getConfiguration() {
+        return configuration;
+    }
+
+
+
+
 }

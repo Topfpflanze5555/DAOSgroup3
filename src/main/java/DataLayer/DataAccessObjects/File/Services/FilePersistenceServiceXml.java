6679da797809a39678aff295fc699bb91c6358c6
Felix Bruemmendorf <felixb1996@gmx.de>
2025-06-03 11:59:49 +0200

    add FilePersistenceServiceXml and XmlWrapper

--- src/main/java/DataLayer/DataAccessObjects/File/Services/FilePersistenceServiceXml.java
+++ src/main/java/DataLayer/DataAccessObjects/File/Services/FilePersistenceServiceXml.java
@@ -1,5 +1,51 @@
 package DataLayer.DataAccessObjects.File.Services;
 
-public class FilePersistenceServiceXml {
+import DataLayer.Exceptions.DAOException;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FilePersistenceServiceXml<T> implements IFIlePersistenceService<T> {
+
+  @Override
+  public List<T> readFile(final Class<T> classType, final Path filePath) throws DAOException
+  {
+    try
+    {
+      JAXBContext context = JAXBContext.newInstance(XmlWrapper.class, classType);
+      Unmarshaller unmarshaller = context.createUnmarshaller();
+      XmlWrapper<T> wrapper = (XmlWrapper<T>) unmarshaller.unmarshal(filePath.toFile());
+      if (wrapper.getItems() == null)
+      {
+        return new ArrayList<>();
+      }
+      return wrapper.getItems();
+    } catch (JAXBException e)
+    {
+      throw new DAOException(e.getMessage());
+    }
+  }
+
+  @Override
+  public void writeFile(final Class<T> classType, final List<T> listToPersist, final Path filePath) throws DAOException
+  {
+    try
+    {
+      JAXBContext context = JAXBContext.newInstance(XmlWrapper.class, classType);
+      Marshaller marshaller = context.createMarshaller();
+      marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+      XmlWrapper<T> wrapper = new XmlWrapper<>(listToPersist);
+      marshaller.marshal(wrapper, filePath.toFile());
+    } catch (JAXBException e)
+    {
+      throw new DAOException(e.getMessage());
+    }
+
+  }
 }
--- src/main/java/DataLayer/DataAccessObjects/File/Services/XmlWrapper.java
+++ src/main/java/DataLayer/DataAccessObjects/File/Services/XmlWrapper.java
@@ -1,4 +1,35 @@
 package DataLayer.DataAccessObjects.File.Services;
 
-public class XmlWrapper {
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+
+@XmlRootElement(name = "items")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class XmlWrapper<T> {
+
+  @XmlAnyElement
+  private List<T> items;
+
+  public XmlWrapper()
+  {
+  }
+
+  public XmlWrapper(List<T> items)
+  {
+    this.items = items;
+  }
+
+  public List<T> getItems()
+  {
+    return items;
+  }
+
+  public void setItems(final List<T> items)
+  {
+    this.items = items;
+  }
 }

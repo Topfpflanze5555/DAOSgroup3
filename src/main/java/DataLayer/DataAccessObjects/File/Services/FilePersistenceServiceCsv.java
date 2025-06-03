c684b0afea504ad2df49d3f16ec46ae683d253d7
Felix Bruemmendorf <felixb1996@gmx.de>
2025-06-03 09:10:57 +0200

    add methods in FilePersistenceServiceCsv

--- src/main/java/DataLayer/DataAccessObjects/File/Services/FilePersistenceServiceCsv.java
+++ src/main/java/DataLayer/DataAccessObjects/File/Services/FilePersistenceServiceCsv.java
@@ -1,4 +1,30 @@
 package DataLayer.DataAccessObjects.File.Services;
 
-public class FilePersistenceServiceCsv {
+import DataLayer.Exceptions.DAOException;
+
+import java.nio.file.Path;
+import java.util.List;
+
+
+public class FilePersistenceServiceCsv<T> implements IFIlePersistenceService<T> {
+
+  private char separator;
+
+  public FilePersistenceServiceCsv(char separator)
+  {
+
+  }
+
+  @Override
+  public List<T> readFile( Class<T> classType,Path filePath) throws DAOException
+  {
+    return List.of();
+  }
+
+  @Override
+  public void writeFile(Class<T> classType,List<T> listToPersist, final Path filePath)
+    throws DAOException
+  {
+
+  }
 }

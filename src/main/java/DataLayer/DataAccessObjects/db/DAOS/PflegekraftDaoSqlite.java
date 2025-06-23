8faba4b07619364890879e55b827e1e9dbe9cedb
Felix Bruemmendorf <felixb1996@gmx.de>
2025-06-23 14:21:19 +0200

    implement methods in PflegekraftDaoSqlite

--- src/main/java/DataLayer/DataAccessObjects/db/DAOS/PflegekraftDaoSqlite.java
+++ src/main/java/DataLayer/DataAccessObjects/db/DAOS/PflegekraftDaoSqlite.java
@@ -2,40 +2,99 @@ package DataLayer.DataAccessObjects.db.DAOS;
 
 import Models.Pflegekraft;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import DataLayer.DataAccessObjects.IDao;
 
-public class PflegekraftDaoSqlite implements IDao<Pflegekraft, Long> {
+
+public class PflegekraftDaoSqlite extends AbstractDaoSqlite<Pflegekraft, Long> implements IDao<Pflegekraft, Long>
+{
 
 	@Override
-	public Pflegekraft create(Pflegekraft t) {
-		// TODO Auto-generated method stub
-		return null;
+	protected String getTableName()
+	{
+		return "Pflegekraft";
 	}
 
 	@Override
-	public Pflegekraft read(Long Id) {
-		// TODO Auto-generated method stub
-		return null;
+	protected String getPrimaryKeyColumn()
+	{
+		return "id";
 	}
 
 	@Override
-	public List<Pflegekraft> read() {
-		// TODO Auto-generated method stub
-		return null;
+	protected String getSqlCreateTableIfNotExists()
+	{
+      return "CREATE TABLE IF NOT EXISTS pflegekraft (\n" +
+          "    id INTEGER PRIMARY KEY AUTOINCREMENT,\n" +
+          "    vorname TEXT NOT NULL,\n" +
+          "    nachname TEXT NOT NULL,\n" +
+          "    geburtsdatum REAL NOT NULL,\n" +
+          "    pflegegrad INTEGER,\n" +
+          "    zimmer INTEGER,\n" +
+          "    vermoegen INTEGER\n" +
+          ")";
 	}
 
 	@Override
-	public void update(Pflegekraft t) {
-		// TODO Auto-generated method stub
-		
+	protected String getSqlInsert()
+	{
+		return "INSERT INTO"
+			+ getTableName()
+			+
+			"(id, nachname, vorname, telefon)" +
+			" VALUES(?,?,?,?)";
 	}
 
 	@Override
-	public void delete(Long Id) {
-		// TODO Auto-generated method stub
-		
+	protected String getSqlUpdate()
+	{
+		return "UPDATE " + getTableName() +
+			" SET nachname = ?, vorname = ?, telefon = ?" +
+			" WHERE id = ?";
 	}
-    
+
+	@Override
+	protected Pflegekraft mapResultSetToObject(ResultSet resultSet)
+	{
+		Pflegekraft pflegekraft = new Pflegekraft();
+		try {
+			pflegekraft.setId(resultSet.getLong(getPrimaryKeyColumn()));
+			pflegekraft.setNachname(resultSet.getString("nachname"));
+			pflegekraft.setVorname(resultSet.getString("vorname"));
+			pflegekraft.setTelefon(resultSet.getString("telefon"));
+		}
+        catch (SQLException e)
+        {
+          throw new RuntimeException(e);
+        }
+		return pflegekraft;
+
+    }
+
+	@Override
+	protected void setInsertStatement(final PreparedStatement preparedStatement,
+									  final Pflegekraft objectToInsert)
+	{
+
+
+	}
+
+	@Override
+	protected void setGeneratedIdToObject(final PreparedStatement preparedStatement,
+										  final Pflegekraft objectToInsert)
+	{
+
+	}
+
+	@Override
+	protected void setUpdateStatement(final PreparedStatement preparedStatement,
+									  final Pflegekraft objectToUpdate)
+	{
+
+	}
 }

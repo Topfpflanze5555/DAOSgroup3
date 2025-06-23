cea09ca90b75cca1027b68ff75059c13e89067f0
Topfpflanze5555 <Lars.Wellbrock@ewe.net>
2025-06-23 14:20:53 +0200

    implement LeistungDaoSqlite.java

--- src/main/java/DataLayer/DataAccessObjects/db/DAOS/LeistungDaoSqlite.java
+++ src/main/java/DataLayer/DataAccessObjects/db/DAOS/LeistungDaoSqlite.java
@@ -2,40 +2,108 @@ package DataLayer.DataAccessObjects.db.DAOS;
 
 import Models.Leistung;
 
-import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import DataLayer.DataAccessObjects.IDao;
+public class LeistungDaoSqlite extends AbstractDaoSqlite<Leistung,String>  {
 
-public class LeistungDaoSqlite implements IDao<Leistung, String> {
+
 
 	@Override
-	public Leistung create(Leistung t) {
-		// TODO Auto-generated method stub
-		return null;
+	protected String getTableName() {
+		return "leistung";
 	}
 
 	@Override
-	public Leistung read(String Id) {
-		// TODO Auto-generated method stub
-		return null;
+	protected String getPrimaryKeyColumn() {
+		return "lkNr";
 	}
 
 	@Override
-	public List<Leistung> read() {
-		// TODO Auto-generated method stub
-		return null;
+	protected String getSqlCreateTableIfNotExists() {
+		return """
+                CREATE TABLE IF NOT EXISTS leistung (
+                    lkNr TEXT PRIMARY KEY,
+                    bezeichnung TEXT,
+                    beschreibung TEXT
+                )""";
+
+
 	}
 
 	@Override
-	public void update(Leistung t) {
-		// TODO Auto-generated method stub
-		
+	protected String getSqlInsert() {
+		return "INSERT INTO " + getTableName() + " ("+getPrimaryKeyColumn()+", bezeichnung, beschreibung) VALUES (?, ?)";
 	}
 
 	@Override
-	public void delete(String Id) {
-		// TODO Auto-generated method stub
-		
+	protected String getSqlUpdate() {
+		return "SET "+getTableName()+" = ?, bezeichnung = ?, beschreibung = ? WHERE " + getPrimaryKeyColumn() + " = ?";
 	}
 
+	@Override
+	protected String getSqlReadId() {
+		return "";
+	}
+
+	@Override
+	protected String getSqlReadAll() {
+		return "";
+	}
+
+	@Override
+	protected String getSqlDelete() {
+		return "";
+	}
+
+	@Override
+	protected Leistung mapResultSetToObject(ResultSet resultSet) {
+
+		Leistung leistung = new Leistung();
+		try {
+			leistung.setBeschreibung(resultSet.getString("beschreibung"));
+			leistung.setBezeichnung(resultSet.getString("bezeichnung"));
+			leistung.setLkNr(resultSet.getString(getPrimaryKeyColumn()));
+
+		} catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+		return leistung;
+    }
+	private void mapPreparedStatementParameters(Leistung leistung, PreparedStatement preparedStatement) {
+		String bezeichnung = leistung.getBezeichnung();
+		String beschreibung = leistung.getBeschreibung();
+
+
+		try {
+			preparedStatement.setString(1, bezeichnung);
+			preparedStatement.setString(2, beschreibung);
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	protected void setInsertStatement(PreparedStatement preparedStatement, Leistung objectToInsert) {
+		mapPreparedStatementParameters(objectToInsert, preparedStatement);
+
+	}
+
+	@Override
+	protected void setGeneratedIdToObject(PreparedStatement preparedStatement, Leistung objectToInsert) {
+
+		try {
+			objectToInsert.setLkNr(preparedStatement.getGeneratedKeys().getString(1));
+		}  catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	protected void setUpdateStatement(PreparedStatement preparedStatement, Leistung objectToUpdate) {
+		mapPreparedStatementParameters(objectToUpdate, preparedStatement);
+	}
 }

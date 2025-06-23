add8fa6162fe19f8732e1abb1e1e786726c3c2cd
AtIgnister <137562253+AtIgnister@users.noreply.github.com>
2025-06-23 09:01:52 +0200

    add create function

--- src/main/java/DataLayer/DataAccessObjects/db/DAOS/PatientDaoSqlite.java
+++ src/main/java/DataLayer/DataAccessObjects/db/DAOS/PatientDaoSqlite.java
@@ -1,7 +1,12 @@
 package DataLayer.DataAccessObjects.db.DAOS;
 
+import DataLayer.DataAccessObjects.db.DAOS.services.ConnectionManager;
+import DataLayer.DataAccessObjects.db.DAOS.services.ConnectionManagerSqlite;
 import Models.Patient;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,16 +15,35 @@ import DataLayer.DataAccessObjects.IDao;
 public class PatientDaoSqlite implements IDao<Patient, Long> {
 
 	@Override
-	public Patient create() {
-		// TODO Auto-generated method stub
+	public Patient create(Patient t) {
+
+		ConnectionManager conMan = new ConnectionManagerSqlite();
+		String stmt = "INSERT INTO patienten (vorname, nachname, pflegegrad, zimmer, vermoegen) VALUES (?, ?, ?, ?, ?)";
+
+		String vorname = t.getVorname();
+		String nachname = t.getNachname();
+		Integer pflegegrad = t.getPflegegrad();
+		String zimmer = t.getZimmer();
+		int vermoegen = (int)t.getVermoegen()*100;
+
+		try {
+			Connection conn = conMan.getNewConnection();
+			PreparedStatement pStmt = conn.prepareStatement(stmt);
+			pStmt.setString(1, vorname);
+			pStmt.setString(2, nachname);
+			pStmt.setInt(3, pflegegrad);
+			pStmt.setString(4, zimmer);
+			pStmt.setInt(5, vermoegen);
+
+			pStmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
+
 		return null;
 	}
 
-	@Override
-	public Patient create(Patient t) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public Patient read(Long Id) {

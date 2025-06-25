e3a95406ea57c4e847f4ee3e9e2051eba17d7f87
Topfpflanze5555 <Lars.Wellbrock@ewe.net>
2025-06-25 10:34:54 +0200

    add interface to DataLayer.java

--- src/main/java/DataLayer/Services/DataLayer.java
+++ src/main/java/DataLayer/Services/DataLayer.java
@@ -1,10 +1,10 @@
 package DataLayer.Services;
 import DataLayer.DataAccessObjects.IDao;
 import Models.*;
-public class DataLayer {
+public class DataLayer implements  IDataLayer{
 	private IDao<Leistung, String> daoLeistung;
 	private IDao<Patient, Long> daoPatient;
-	private IDao<Pflegekraft, String> daoPflegekraft;
+	private IDao<Pflegekraft, Long> daoPflegekraft;
 	
 	protected void setDaoLeistung(IDao<Leistung, String> daoLeistung)
 	{
@@ -14,8 +14,28 @@ public class DataLayer {
 	{
 		this.daoPatient = daoPatient;
 	}
-	protected void setDaoPflegekraft(IDao<Pflegekraft, String> daoPflegekraft)
+	protected void setDaoPflegekraft(IDao<Pflegekraft, Long> daoPflegekraft)
 	{
 		this.daoPflegekraft = daoPflegekraft;
+	}
+
+	@Override
+	public String toString() {
+		return "DataLayer [daoLeistung=" + daoLeistung + ", daoPatient="+daoPatient+", daoPflegekraft="+daoPflegekraft+"]";
+	}
+
+	@Override
+	public IDao<Leistung, String> getDaoLeistung() {
+		return daoLeistung;
+	}
+
+	@Override
+	public IDao<Patient, Long> getDaoPatient() {
+		return daoPatient;
+	}
+
+	@Override
+	public IDao<Pflegekraft, Long> getDaoPflegekraft() {
+		return daoPflegekraft;
 	}
 }

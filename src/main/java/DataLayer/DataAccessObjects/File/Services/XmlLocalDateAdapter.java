5728d7222864553ed9bd779cde58a8ea47efd570
Topfpflanze5555 <Lars.Wellbrock@ewe.net>
2025-07-02 12:43:59 +0200

    implement XmlLocalDateAdapter.java

--- src/main/java/DataLayer/DataAccessObjects/File/Services/XmlLocalDateAdapter.java
+++ src/main/java/DataLayer/DataAccessObjects/File/Services/XmlLocalDateAdapter.java
@@ -1,9 +1,33 @@
 package DataLayer.DataAccessObjects.File.Services;
 
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
-public class XmlLocalDateAdapter<String, LocalDate> {
+public class XmlLocalDateAdapter extends XmlAdapter<String, LocalDate> {
 
-  private DateTimeFormatter formatter;
+  private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+  XmlLocalDateAdapter()
+  {
+
+    super();
+    formatter.format(LocalDate.now());
+  }
+  XmlLocalDateAdapter(LocalDate localDate)
+  {
+    super();
+    formatter.format(localDate);
+  }
+
+  @Override
+  public LocalDate unmarshal(String string) throws Exception {
+    return LocalDate.parse(string, formatter);
+  }
+
+  @Override
+  public String marshal(LocalDate localDate) throws Exception {
+    return localDate.format(formatter);
+  }
 }
